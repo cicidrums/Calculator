6640792c003ee57cc4a6346a294e1446f8fe0545
Cici <cici.harrison@gmail.com>
2018-06-05 13:25:18 -0700

    append two numbers, but it still won't add them as numbers. maybe it's because input type is 'text'?

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
 $(document).ready(function() {
 
 	$("form#calc").submit(function(event) {
 		event.preventDefault();
 
-		var number1Input = $("#number1").val();
-		var number2Input = $("#number2").val();
+		var result = add(number1, number2);
+		var number1Input = parseInt($("#number1").val());
+		var number2Input = parseInt($("#number2").val());
 
-		$("span.number1").append(number1Input+"cc");
+		$("span.number1").append(number1Input);
 		$("span.number2").append(number2Input);
 
 		$(".result").show();
