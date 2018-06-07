d09ade49d4332b38b5399165b08ea359dfea15f4
Cici Harrison and Zoe Zhou <cici@>
2018-06-06 18:56:55 -0700

    it will introduce radio inputs allowing you to change math operators, with some styling upgrades, everything works

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,37 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
 $(document).ready(function() {
 
 	$("form#calc").submit(function(event) {
 		event.preventDefault();
 
-		var number1Input = parseInt($("#number1").val());
-		var number2Input = parseInt($("#number2").val());
-    var result = number1Input+number2Input;
+		var number1 = parseInt($("#number1").val());
+		var number2 = parseInt($("#number2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
 
-    console.log(number1Input);
-    console.log(number2Input);
+    if (operator === "add") {
+      var result = add(number1, number2);
+    } else if (operator === "subtract") {
+      var result = subtract(number1, number2);
+    } else if (operator === "divide") {
+      var result = divide(number1, number2);
+    } else if (operator === "multiply") {
+      var result = multiply(number1, number2);
+    }
 
-		$("span.sum").append(result);
+    $("span.result").empty();
+		$("span.result").append(result);
 
     $(".result").show();
 
