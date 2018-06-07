e488bc0cb3c533b62750de02b55ba8ceb6e3d460
Cici Harrison and Zoe Zhou <zoe@>
2018-06-06 19:41:06 -0700

    it will add JS logic for evaluating what type of triangle the three given numbers create and if they result in a triangle at all.

--- js/scripts.js
+++ js/scripts.js
@@ -34,4 +34,31 @@ $(document).ready(function() {
     $(".result").show();
 
 	});
+
+  $("form#tri").submit(function(event) {
+    event.preventDefault();
+
+    var num1 = parseInt($("#num1").val());
+		var num2 = parseInt($("#num2").val());
+    var num3 = parseInt($("#num3").val());
+    var sum1 = num1 + num2;
+    var sum2 = num2 + num3;
+    var sum3 = num3 + num1;
+
+    if (num1 === num2 && num2 === num3 && num1 === num3) {
+      var tri = "Equilateral.";
+    } else if (num1 === num2 || num2 === num3 || num1 === num3) {
+      var tri = "Isosceles.";
+    } else if (sum1 <= num3 || sum2 <= num1 || sum3 <= num2) {
+      var tri = "This is NOT a triangle yo.";
+    } else if (num1 !=num2 && num1 !=num3 && num2 !=num3) {
+      var tri = "Scalene.";
+    }
+
+    $("span.result2").text(tri);
+    $(".result2").show();
+
+
+
+  });
 });
